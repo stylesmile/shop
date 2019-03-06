82baa31c00d1d8ac20c8af8d97c7edbb2cb5be28
chenye <1403539106@qq.com>
2019-03-06 10:56:42 +0800

    登录

--- web-admin/src/components/login.vue
+++ web-admin/src/components/login.vue
@@ -2,18 +2,23 @@
   <div class="login">
     <el-row style="margin-top: 260px;z-index: 1;">
       <el-col :model="user" :offset="9" :span="6">
-        <el-card class="login-box" v-loading="$store.state.loading" element-loading-background="rgba(0, 0, 0, 0.8)">
+        <!--<el-card class="login-box" v-loading="$store.state.loading" element-loading-background="rgba(0, 0, 0, 0.8)">-->
+        <el-card class="login-box" v-loading="" element-loading-background="rgba(0, 0, 0, 0.8)">
           <el-form>
-            <h1 class="title">欢迎使用Element Admin</h1>
-            <p class="login-box-msg">疾如风，徐如林!</p>
+            <h1 class="title">欢迎使用web-admin</h1>
             <el-form-item>
-              <el-input type="text" v-model="user.userName" auto-complete="off" placeholder="请输入用户名..." suffix-icon="el-icon-bell"></el-input>
+              <el-input type="text" v-model="user.userName" auto-complete="off" placeholder="请输入用户名..."
+                        suffix-icon="el-icon-bell"></el-input>
             </el-form-item>
             <el-form-item>
-              <el-input type="password" v-model="user.password" auto-complete="off" placeholder="请输入密码..." suffix-icon="el-icon-edit"></el-input>
+              <el-input type="password" v-model="user.password" auto-complete="off" placeholder="请输入密码..."
+                        suffix-icon="el-icon-edit"></el-input>
             </el-form-item>
             <el-form-item>
-              <el-button type="primary" @click="login" :loading="$store.state.loading" class="pull-right" style="width:100%">登录</el-button>
+              <!--<el-button type="primary" @click="login" :loading="$store.state.loading" class="pull-right"-->
+              <el-button type="primary" @click="login"  class="pull-right"
+                         style="width:100%">登录
+              </el-button>
             </el-form-item>
           </el-form>
         </el-card>
@@ -22,49 +27,49 @@
   </div>
 </template>
 <style>
-.login {
-  background: url(../assets/bg.png) no-repeat scroll center center / cover;
-  background-size: 100% 100%;
-  width: 100%;
-  height: 100%;
-  position: fixed;
-}
+  .login {
+    background: url(../assets/banner.png) no-repeat scroll center center / cover;
+    background-size: 100% 100%;
+    width: 100%;
+    height: 100%;
+    position: fixed;
+  }
 
-.login-box {
-  background: #ffffff;
-  border: none;
-}
+  .login-box {
+    background: #ffffff;
+    border: none;
+  }
 
-.login-box-msg {
-  color: #000000;
-  text-align: center;
-}
+  .login-box-msg {
+    color: #000000;
+    text-align: center;
+  }
 
-.login-box .title {
-  color: #000000;
-  text-align: center;
-}
+  .login-box .title {
+    color: #000000;
+    text-align: center;
+  }
 </style>
 <script>
 export default {
-  name: "Login",
-  data() {
+  name: 'Login',
+  data () {
     return {
       user: {
-        userName: "",
-        password: ""
+        userName: '',
+        password: ''
       }
-    };
+    }
   },
   methods: {
-    login() {
-      this.$http.post("/passport/signIn", this.user).then(res => {
+    login () {
+      this.$http.post('/passport/signIn', this.user).then(res => {
         if (res.data.token) {
-          this.$store.commit("setToken", res.data.token);
-          this.$router.push({ path: "/dashboard" });
+          this.$store.commit('setToken', res.data.token)
+          this.$router.push({path: '/'})
         }
-      });
+      })
     }
   }
-};
+}
 </script>
--- web-admin/src/router/index.js
+++ web-admin/src/router/index.js
@@ -1,20 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import login from '@/components/login'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(Router)
+Vue.use(ElementUI)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/1',
       name: 'login',
       component: HelloWorld
     },
     {
-      path: '/login',
+      path: '/',
       name: 'login',
-      component: HelloWorld
+      component: () => import('@/components/login.vue')
+    },
+    {
+      path: '/login2',
+      name: 'login',
+      component: login
     }
 
   ]
